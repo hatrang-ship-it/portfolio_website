2d10e546c0ec7c5ee9628c4195d2f521e7cd4cd1
hatrang-ship-it <lehatrang21102004@gmail.com>
2026-05-14 09:34:44 +0700

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Thay 'portfolio' bằng tên repository của bạn trên GitHub
+  base: '/portfolio_website/',
 })
